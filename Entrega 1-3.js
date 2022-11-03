ca579f6faad2d60065fd7c195b1c7e280180841a
edgonga <gonzalez.edu.duc@gmail.com>
2022-11-03 13:00:22 +0100

    atascado en Nivel 2 ej 1

--- Entrega 1-3.js
+++ Entrega 1-3.js
@@ -37,6 +37,118 @@ discoEntrance
     })
     .catch(err => {
         
-        console.log('A la calle')
+        console.log('Vete por donde has venido')
     });
+
+
+
+/*
+-- Exercici 1
+- Nivell 2
+Crea una arrow function que rebi un paràmetre i una funció callback
+ i li passi a la funció un missatge o un altre 
+ (que s'imprimirà per consola) en funció del paràmetre rebut.
+*/
+
+
+function laVidaEsUnaBambola(min_number, max_number, jour, dia) {
+
+    
+    
+        min_number = Math.ceil(min_number);
+        max_number = Math.floor(max_number);
+        jour = new Date();
+        dia = jour.getDay();
+        return Math.floor(Math.random() * (max_number - min_number) + min_number), dia;
+        
+        
+        //Porque no puedo declarar la variable dia dentro de esta función 
+        //y utilizarla como parámetro en la función de ganadorLoteria?
+}
+//Descomentar las dos lineas siguientes para que el código funcione
+const jour = new Date();
+let dia = jour.getDay();
+
+
+ganadorLoteria = (dayOfTheWeek, numGanador) => {
+
+    //hacer el if
+    if (dayOfTheWeek > 6) {
+        numGanador = String(numGanador)
+        console.log(`El número premiado de la loteria es el ${numGanador}`)
+    }   else {
+        console.log('Hoy es fin de semana, hoy no hay sorteo')
+    }
+}
+
+// Faig un delay de 2.5 segons perque aquest exercici no s'executi abans
+// del primer exercici
+
+setTimeout(() => {
+    ganadorLoteria(dia, laVidaEsUnaBambola(1, 1000));
+    }, 2500)
+
+
+
+
+/*
+-- Nivell 2
+- Exercici 1
+Donats els objectes employees i salaries, crea una arrow function 
+getEmployee() que retorni una 
+Promise efectuant la cerca en l'objecte pel seu id.
+*/
+
+let employees = [{
+    id: 1,
+    name: 'Linux Torvalds'
+}, {
+    id: 2,
+    name: 'Bill Gates'
+},{
+    id: 3,
+    name: 'Jeff Bezos'
+}];
+ 
+let salaries = [{
+    id: 1,
+    salary: 4000
+}, {
+    id: 2,
+    salary: 1000
+}, {
+    id: 3,
+    salary: 2000
+}];
+
+//console.log(employees[1][1])
+
+// getEmployee = new Promise((resolve, reject) => {
+//     if (employeeID > 0 && employeeID < 4) {
+        
+//     }
+// })
+
+
+
+var getEmployee = function (employeeID) {
+    return new Promise((resolve, reject) => {
+        
+        if (employeeID > 0 && employeeID < 4) {
+            resolve(console.log('Resolved'))
+                //employees[employeeID]  + " - " + salaries[employeeID]));
+        }   else {
+            reject()//new Error("Este ID no existe"));
+        }
+    })
+}
+
+getEmployee(2)
+    .then(res => {console.log(
+        employees[employeeID]  + " - " + salaries[employeeID]);
+
+    })
+    .catch(err => {console.log('No resolved')
+
+    })
 
