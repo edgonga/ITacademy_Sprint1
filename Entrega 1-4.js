3e892b6e59c382367f000ee7159d734ad58da8d1
edgonga <gonzalez.edu.duc@gmail.com>
2022-11-10 13:30:10 +0100

    nivel 2 ej 1 hecho (intentar hacerlo con Date.now)

--- Entrega 1-4.js
+++ Entrega 1-4.js
@@ -88,23 +88,107 @@ Promise que efectuï la seva funció resolve() després
 de 2 segons de la seva invocació.
 */
 
-async function delayEmployee(idDelayEmployee) 
-    se 
-        try{
-        
-            const delayEmployee = await getEmployee(idDelayEmployee)
-            console.log(delayEmployee)
-        
-        } catch(err) {
-        console.log(err)
 
+
+
+
+
+
+
+const canaryDelay = (peninsulaHour) => {
+    return new Promise((resolve, reject) => {
+        const canaryHour = peninsulaHour
+
+        if(peninsulaHour) { 
+            
+            setTimeout(() => {
+                resolve(canaryHour)
+            }, 2000)
+            
+        
     }
+        else {
+            reject("Error")
+        }
+
+    })
+}
+      
 
 
-delayEmployee(2)
+async function whatTimeIs(currentHour) {
+    try{
+        
+        const asyncPeninsulaHour = await canaryDelay(currentHour)
+        console.log(`En la Peninsula son las ${asyncPeninsulaHour}h`)
+        const asyncCanaryHour = await canaryDelay(currentHour) -1
+        console.log(`Mientras que en las Canarias son las ${asyncCanaryHour}h`)
+    }   catch(err) {
+        console.log(err)
+    }}
 
 
 
-const canaryTimetable = (peninsulaTime) => new Promise(resolve => setTimeout(resolve, (peninsulaTime*1000)- 1000))
+whatTimeIs(12);
+/*
+-- Nivell 2
+- Exercici 1
+Crea una funció que retorni el doble 
+del número que li passa com a paràmetre després de 2 segons.
 
-const 
+Crea una altra funció que rebi tres números i calculi la suma 
+dels seus dobles fent servir la funció anterior.
+*/
+
+const dailyDay = (bitcoinValue) => {
+    return new Promise((resolve, reject) => {
+        
+
+        if(bitcoinValue) {
+            setTimeout(() => {
+                resolve(bitcoinValue)
+            }, 2000)
+    }
+        else {
+            reject("El bitcoin es una estafa piramidal")
+        }
+    })
+}
+
+
+async function elMercadoDeLaCripto(bitcoin) {
+    try{
+        const yesterdayBitcoin = await dailyDay(bitcoin)
+        //const newDate = new Date
+        //const today = () => console.log(Date.now())
+        //const yesterday = () => console.log(today.getDate() -1);
+        //console.log(`${yesterday()} --> precio de bitcoin era ${yesterdayBitcoin}€`)
+        console.log(`Ayer el precio del bitcoin era de ${yesterdayBitcoin}€`)
+        const todayBitcoin = await dailyDay(bitcoin) * 2
+        //console.log(`${today()} --> precio del bitcoin es de ${todayBitcoin}€`)
+        console.log(`Hoy el precio del bitcoin es de ${todayBitcoin}€`)
+    }   catch(err) {
+        console.log(err)
+    }}
+
+
+elMercadoDeLaCripto(20000)
+
+
+
+
+
+
+
+
+// bitcoinValue = 20000
+// console.log(bitcoinValue);
+
+// const elMercadoDeLaCripto = (bitcoinPrice) = async () => {
+//     console.log(`Ayer día 9 de noviembre, el precio de bitcoin era ${bitcoinPrice}€`)
+//     await dailyDay(bitcoinPrice)
+//     //bitcoinPrice = bitcoinPrice * 2
+//     console.log(`Hoy día 10 de noviembre, el precio del bitcoin es de ${bitcoinPrice}€`)
+// }
+
+// elMercadoDeLaCripto(bitcoinValue);
