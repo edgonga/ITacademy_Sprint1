49f19df1cd21747bc94ef5ce13bbd271492b43c6
edgonga <gonzalez.edu.duc@gmail.com>
2022-11-14 13:07:40 +0100

    understanding testing / TDD

---
+++ Entrega 1-6/app.js
@@ -0,0 +1,9 @@
+/* 
+-- Nivell 1
+Crea un arxiu amb les funcions sumar, restar, multiplicar 
+i dividir dos o més operands. 
+Testeja la correcta execució d'aquestes funcions.
+*/
+
+// OPERATIONS
+
