776890d1bdec8a5623f6747aea3eadfc8b9c5884
edgonga <gonzalez.edu.duc@gmail.com>
2022-12-01 12:31:57 +0100

    ex codificar 1r part almost done

--- Entrega 1-5.js
+++ Entrega 1-5.js
@@ -86,10 +86,56 @@ d'usuari/ària de l'ordinador utilizant Node Child Processes.
 
 accessDirOS = () => { 
     const { exec } = require('child_process')
-    exec('dir', (error, stdout, stderr) => {
-        if(error) {
-            console.log(error)
-        }
-        console.log(stdout)
-    })
+    const os = require('os')
+    userDirectory = os.homedir()
+        if (process.env.OS.includes("Windows")) {
+        exec(`dir ${userDirectory}`, (error, stdout, stderr) => {
+                if(error) {
+                    console.log(error)
+                }
+                console.log(stdout)
+            })
+        } 
+        if (process.env.OS.includes("Mac")) {
+            exec(`${userDirectory} ls`, (error, stdout, stderr) => {
+                if(error) {
+                    console.log(error)
+                }
+                console.log(stdout)
+            })
+        }  
+    }
+
+accessDirOS()
+
+
+/*
+-- Nivell 3
+Crea una funció que creï dos fitxers codificats en hexadecimal 
+i en base64 respectivament, a partir del fitxer del nivell 1.
+*/
+
+const { read } = require('fs');
+const fs = require('fs');
+
+fileEncoder = (fileToBase64) => {
+    
+    InputDataInsideTheFile = fs.readFileSync(fileToBase64, 'utf8')
+    console.log(`El string sin codificar es ${InputDataInsideTheFile}`)
+    const buff = new Buffer.from(InputDataInsideTheFile)
+    const base64file = buff.toString('base64')
+    outputText = fs.writeFileSync(fileToBase64, base64file)
+    setTimeout(() => {  // No entiendo porque me devuelve un undefined y no el string codificado, aunque espere 2 segundos por si el proceso de codificación tarda mucho :(
+        console.log(`El string codificado es ${outputText}`)
+    }, 2000)
 }
+
+fileEncoder('./helloworld.txt')
+
+/* 
+------Chunks------
+1ro - Coger el texto actual que hay en el archivo en cuestión (helloworld.txt)
+2do - Codificar ese texto a base64 (y más tarde a hex)
+3ro - Escribir el texto codificado otra vez al archivo en cuestión (helloworld.txt) 
+*/
+
