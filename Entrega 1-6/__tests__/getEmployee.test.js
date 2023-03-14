c344d49ac9ad843aec3535d5544807e51611048c
Edu Gonzalez Garcia <71343560+edgonga@users.noreply.github.com>
2023-03-14 13:34:15 +0100

    14032023

--- Entrega 1-6/__tests__/getEmployee.test.js
+++ Entrega 1-6/__tests__/getEmployee.test.js
@@ -1,12 +1,24 @@
 const getEmployee = require('../../Entrega 1-3')
+const testData = 1
 
-test('el paràmetre està entre 1 i 3', () => {
-    expect(getEmployee(2)).toBe(salarySelected)
- })
+test('el paràmetre està entre 1 i 3 i la promise es resolt correctament', () => {
+   return getEmployee(1).then(value => {
+      expect(value).toBe({name: 'Linux Torvalds'})
+   })
+   })
 
 test('el paràmetre NO està entre 1 i 3 i per tant es fara un console.log del error', () => {
-    expect(getEmployee(5)).toEqual('Debes introducir un id del 1 al 3')
- })
+   const getEmployee = Promise.reject("Failed")
+   return expect(getEmployee).rejects.toBe("Failed")
+   })
+
+
+// test('el paràmetre NO està entre 1 i 3 i per tant es fara un console.log del error', () => {
+//    const checkEmployee = getEmployee(4).then(inputData => {
+//       expect(inputData).toBe("Error: Failed")
+//    })
+//    return checkEmployee
+//  })
 
  test('el paràmetre es un string i no un número, per tant es fara un console.log del error', () => {
     expect(getEmployee('Qatar')).toEqual('Este id no existe')
