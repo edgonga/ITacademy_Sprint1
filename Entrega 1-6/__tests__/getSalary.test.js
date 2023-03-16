9a5cf5c87c3c52efaeb59022c13845b431754f15
Edu Gonzalez Garcia <71343560+edgonga@users.noreply.github.com>
2023-03-16 13:45:26 +0100

    Update getSalary.test.js

--- Entrega 1-6/__tests__/getSalary.test.js
+++ Entrega 1-6/__tests__/getSalary.test.js
@@ -1,14 +1,15 @@
-const promiseFile = require('../../Entrega 1-3')
+const promiseFile = require('../app/getSalary')
+const employeeFile = require('../app/getEmployee')
 
 test('el objeto que recibe está dentro del rango de 1 a 3', () => {
    for (index in promiseFile.salaries) {   
-      return promiseFile.getSalary(promiseFile.employees[index]).then(resolve => {
+      return promiseFile.getSalary(employeeFile.employees[index]).then(resolve => {
          expect(resolve).toBe(promiseFile.salaries[index].salary)
       })}
       })
 
 test('el objeto que recibe está fuera del rango correspondiente o el parámetro no es un objeto', () => {
-   return promiseFile.getSalary(promiseFile.employees[6]).catch(reject => {
+   return promiseFile.getSalary(employeeFile.employees[6]).catch(reject => {
       expect(reject).toBe("El parámetro no es un objeto o el índice no existe")
    })
 })
