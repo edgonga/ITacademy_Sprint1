e5c4860505e5043e8ff4b2d0c46eb323e6ee39e5
Edu Gonzalez Garcia <71343560+edgonga@users.noreply.github.com>
2023-03-16 13:45:06 +0100

    2

--- Entrega 1-6/__tests__/getEmployee.test.js
+++ Entrega 1-6/__tests__/getEmployee.test.js
@@ -1,4 +1,4 @@
-const promiseFile = require('../../Entrega 1-3')
+const promiseFile = require('../app/getEmployee')
 
 test('el paràmetre està entre 1 i 3 i la promise es resolt correctament', () => {
    for (index in promiseFile.employees) {   
@@ -20,20 +20,3 @@ test('el parámetro no es un number, por lo tanto se rechaza la promise con el m
 })
 
 
-
-test('el paràmetre NO està entre 1 i 3 i per tant es fara un console.log del error', () => {
-   const getEmployee = Promise.reject("Failed")
-   return expect(getEmployee).rejects.toBe("Failed")
-   })
-
-
-test('el paràmetre NO està entre 1 i 3 i per tant es fara un console.log del error', () => {
-   const checkEmployee = getEmployee(4).then(inputData => {
-      expect(inputData).toBe("Error: Failed")
-   })
-   return checkEmployee
- })
-
- test('el paràmetre es un string i no un número, per tant es fara un console.log del error', () => {
-    expect(getEmployee('Qatar')).toEqual('Este id no existe')
- })
