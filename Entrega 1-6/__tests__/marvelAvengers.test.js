3602f9a3185c260cfcdf2444fca5e2f6cbd51e2a
Edu Gonzalez Garcia <gonzalez.edu.duc@gmail.com>
2023-03-21 13:10:13 +0100

    21032023

--- Entrega 1-6/__tests__/marvelAvengers.test.js
+++ Entrega 1-6/__tests__/marvelAvengers.test.js
@@ -6,26 +6,28 @@ de la classe abstracta de l'exercici Classes & Arrow Functions N3 E1.
 const {MarvelAvengers, heroCreater} = require('../app/marvelAvengers')
 
 
-describe("Creamos un nuevo objeto de la clase marvelAvengers", () => {
-    const thor = new heroCreater();
+describe("Marvel Avengers", () => {
+    const avengerDC = heroCreater('Iron Man', 'Genius', 'Mortality')
+    const avenger = new MarvelAvengers('Iron Man', 'Genius', 'Mortality')
+    const properties = avenger.getHeroProperties()
 
-
-    test("definir método displayName()", () => {   // https://dev.to/dstrekelj/how-to-test-classes-with-jest-jif   
-                                                 
-        expect(typeof thor.displayName).toBe("function")
+    test("Testear que las propiedades de la clase se retornan correctamente con el método getHeroProperties()", () => {   // https://dev.to/dstrekelj/how-to-test-classes-with-jest-jif   
+        expect(properties.heroName).toEqual("Iron Man")
+        expect(properties.heroSuperpower).toEqual("Genius")
+        expect(properties.heroWeakness).toEqual("Mortality")                                     
     })
 })
 
 
-test("displayName() devuelve undefined cuando es llamada sin pasar parámetros cuando hemos instanciado thor", () => {
-    expect(thor.displayName()).toBeUndefined()
-})
+// test("displayName() devuelve undefined cuando es llamada sin pasar parámetros cuando hemos instanciado thor", () => {
+//     expect(thor.displayName()).toBeUndefined()
+// })
 
 
-describe("Creamos el segundo nuevo objeto de la clase marvelAvengers", () => {
-    const ironman = new heroCreater('Ironman');
+// describe("Creamos el segundo nuevo objeto de la clase marvelAvengers", () => {
+//     const ironman = new heroCreater('Ironman');
 
-    test("displayName() devuelve el nombre del héroe cuando es llamada  pasando parámetros al instanciar ironman", () => {
-        expect(ironman.displayName()).toBe('Ironman')
-    })
-})
+//     test("displayName() devuelve el nombre del héroe cuando es llamada  pasando parámetros al instanciar ironman", () => {
+//         expect(ironman.displayName()).toBe('Ironman')
+//     })
+// })
